73e0f4c12fe37ca67ebdfd3181e7cc0a0c7e7018
cbonesteel <cambones12@gmail.com>
2021-09-16 16:18:06 -0400

    the algorithm works; made glider gun; looping is broken and so is the array vs window size

--- application.py
+++ application.py
@@ -3,11 +3,12 @@ pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-window_width = 960
-window_height = 540
+red = (255, 0, 0)
+window_width = 1440
+window_height = 820
 
-rows = 54
-cols = 96
+rows = 82
+cols = 144
 
 currentGen = [[0] * cols for _ in range(rows)]
 nextGen = [[0] * cols for _ in range(rows)]
@@ -18,19 +19,76 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((window_width, window_height))
     clock = pygame.time.Clock()
+    done = False
+
     screen.fill(white)
 
-    while True:
+    currentGen[5][5] = 1
+    currentGen[5][6] = 1
+    currentGen[6][6] = 1
+    currentGen[6][5] = 1
+
+    currentGen[5][15] = 1
+    currentGen[6][15] = 1
+    currentGen[7][15] = 1
+
+    currentGen[4][16] = 1
+    currentGen[8][16] = 1
+
+    currentGen[3][17] = 1
+    currentGen[9][17] = 1
+    currentGen[3][18] = 1
+    currentGen[9][18] = 1
+
+    currentGen[6][19] = 1
+
+    currentGen[4][20] = 1
+    currentGen[8][20] = 1
+
+    currentGen[5][21] = 1
+    currentGen[6][21] = 1
+    currentGen[7][21] = 1
+
+    currentGen[6][22] = 1
+
+    currentGen[3][25] = 1
+    currentGen[4][25] = 1
+    currentGen[5][25] = 1
+
+    currentGen[3][26] = 1
+    currentGen[4][26] = 1
+    currentGen[5][26] = 1
+
+    currentGen[2][27] = 1
+    currentGen[6][27] = 1
+
+    currentGen[1][29] = 1
+    currentGen[2][29] = 1
+    currentGen[6][29] = 1
+    currentGen[7][29] = 1
+
+    currentGen[3][39] = 1
+    currentGen[3][40] = 1
+    currentGen[4][40] = 1
+    currentGen[4][39] = 1
+    
+    while not done:
+        clock.tick(10)
+        
         screen.fill(white)
         drawGrid()
-        # drawGeneration()
+        drawCurrentGen()
+        findNextGen()
+        updateCurrentGen()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        
+                done = True
+                
         pygame.display.update()
 
+    pygame.quit()
+
+    
 def drawGrid():
     blockSize = 10 #Set the size of the grid block
     for x in range(0, window_width, blockSize):
@@ -38,6 +96,14 @@ def drawGrid():
             rect = pygame.Rect(x, y, blockSize, blockSize)
             pygame.draw.rect(screen, black, rect, 1)
 
+def drawCurrentGen():
+    blockSize = 10
+    for x in range(0, window_height, blockSize):
+        for y in range(0, window_width, blockSize):
+            if currentGen[int(x/10)][int(y/10)] == 1:
+                rect = pygame.Rect(y, x, blockSize, blockSize)
+                pygame.draw.rect(screen, black, rect, 0)
+                            
 def findNumAdjacent(row, col):
     counter = 0
 
@@ -45,7 +111,7 @@ def findNumAdjacent(row, col):
     rowDown = row + 1
     colLeft = col - 1
     colRight = col + 1
-
+    
     top = False
     bot = False
     left = False
@@ -62,7 +128,7 @@ def findNumAdjacent(row, col):
         top = True
         counter += currentGen[rows - 1][col]
         counter += currentGen[rowDown][col]
-    elif rowUp > rows - 1:
+    elif rowDown > rows - 1:
         bot = True
         counter += currentGen[0][col]
         counter += currentGen[rowUp][col]
@@ -113,9 +179,9 @@ def findNextGen():
                 elif neighbors > 3:
                     nextGen[i][j] = 0
                 else:
-                    nextGen[i][j] = 0
+                    nextGen[i][j] = 1
             else:
-                if nieghbors == 3:
+                if neighbors == 3:
                     nextGen[i][j] = 1
 
 def updateCurrentGen():
