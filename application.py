b98c4a1593aaf19b90c0e54c84306f02fbcab380
cbonesteel <cambones12@gmail.com>
2021-09-17 10:19:51 -0400

    created startPositions.py; moved gliderGun coordinates to startPositions.py

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import sys, pygame
+import startPositions
 pygame.init()
 
 white = (255, 255, 255)
@@ -23,54 +24,7 @@ def main():
 
     screen.fill(white)
 
-    currentGen[5][5] = 1
-    currentGen[5][6] = 1
-    currentGen[6][6] = 1
-    currentGen[6][5] = 1
-
-    currentGen[5][15] = 1
-    currentGen[6][15] = 1
-    currentGen[7][15] = 1
-
-    currentGen[4][16] = 1
-    currentGen[8][16] = 1
-
-    currentGen[3][17] = 1
-    currentGen[9][17] = 1
-    currentGen[3][18] = 1
-    currentGen[9][18] = 1
-
-    currentGen[6][19] = 1
-
-    currentGen[4][20] = 1
-    currentGen[8][20] = 1
-
-    currentGen[5][21] = 1
-    currentGen[6][21] = 1
-    currentGen[7][21] = 1
-
-    currentGen[6][22] = 1
-
-    currentGen[3][25] = 1
-    currentGen[4][25] = 1
-    currentGen[5][25] = 1
-
-    currentGen[3][26] = 1
-    currentGen[4][26] = 1
-    currentGen[5][26] = 1
-
-    currentGen[2][27] = 1
-    currentGen[6][27] = 1
-
-    currentGen[1][29] = 1
-    currentGen[2][29] = 1
-    currentGen[6][29] = 1
-    currentGen[7][29] = 1
-
-    currentGen[3][39] = 1
-    currentGen[3][40] = 1
-    currentGen[4][40] = 1
-    currentGen[4][39] = 1
+    startPositions.setGliderGun(currentGen)
     
     while not done:
         clock.tick(10)
